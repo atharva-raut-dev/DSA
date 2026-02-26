647e815d2e8145154e09fe23e2c854f41529abae
Atharva .S. Raut <rautatharva431@gmail.com>
2026-02-26 05:29:03 +0000

    array questions started

--- 02_FunctionsMethods/BinToDecimal.java
+++ 02_FunctionsMethods/BinToDecimal.java
@@ -1,3 +1,24 @@
+<<<<<<< HEAD
+class BinToDecimal{
+    public static void  binaryToDecimal(int n){
+        int MyNum = n;
+        int pow = 0;
+        int decimal = 0;
+        while(n>0){
+            int LastDigt = n % 10;
+            decimal = decimal + (int)(LastDigt * Math.pow(2,pow));
+            pow ++;
+            n = n / 10;
+        }
+        System.out.print("The conversion of"+MyNum+"to decimal is : "+decimal);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        binaryToDecimal(n);
+    }
+}
+=======
 import java.util.*;
 class BinToDecimal{
     public static void  binaryToDecimal(int n){
@@ -19,3 +40,4 @@ class BinToDecimal{
         binaryToDecimal(n);
     }
 }
+>>>>>>> b3aebe209cee32613a2221740deea33532f86672
